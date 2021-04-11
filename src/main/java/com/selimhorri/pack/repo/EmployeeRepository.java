e8157d839ef06a7c7f3a726daae484894ac9f816
SelimHorri <horri.slim@gmail.com>
2021-04-11 13:49:01 +0100

    create service layer

--- src/main/java/com/selimhorri/pack/repo/EmployeeRepository.java
+++ src/main/java/com/selimhorri/pack/repo/EmployeeRepository.java
@@ -1,11 +1,9 @@
 package com.selimhorri.pack.repo;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
 import com.selimhorri.pack.entity.Employee;
 
-@Repository
 public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
 	
 	
